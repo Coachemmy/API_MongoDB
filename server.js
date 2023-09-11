1af403e09899ef68d722f74bbee689d692eba872
coachemmy <coachemmyb@gmail.com>
2023-09-11 11:56:57 +0100

    api with mongodb and postman

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 require ('dotenv').config()
 const port = process.env.PORT || 3000 
-const Joi = require('joi')
-const express = require('express')
-
+const express = require('express') 
+const Product = require('./model/productModel')
+const mongoose = require('mongoose')
 const app = express()
-
 app.use(express.json())
 
 //these are the courses 
@@ -16,6 +15,7 @@ const courses = [
 
 //get listening port on our browser
 app.get('/', (req, res) =>{
+    
     res.send('Hello World, thanks For Listening')
 })
 
@@ -70,9 +70,83 @@ app.delete('/api/courses/:id', (req, res) => {
     res.send(course)
 })
 
+
+////////////////////////////////////////////////////////////////////////////////////////
+//GET all products
+app.get('/product', async(req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})   
+    }
+})
+
+//GET a single product
+app.get('/product/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findById(id)
+        res.status(200).json(product)
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})  
+    }
+})
+
+//POST products
+app.post('/product', async(req, res) => {
+    try{
+        const products = await Product.create(req.body)
+        res.status(200).json(products)
+
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+})
+
+//PUT a product
+app.put('/product/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        if(!product) return res.status(404).json({message: 'Product not found'})
+        const updatedProduct = await Product.findById(id)
+        res.status(200).json(updatedProduct)
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});  
+    }
+})
+
+//DELETE a product
+app.delete('/product/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findByIdAndDelete(id)
+        if(!product) return res.status(404).json({message: 'Product not found'})
+        res.status(200).json(product)
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        
+    }
+})
+///////////////////////////////////////////////////////////////////
+
+
 const ifTooMuch = () => {
     if(!req.body.name || !req.body.department || req.body.name < 3)
     return res.status(400).send('Please enter a name/department/name left not less than 3')
 }
 
-app.listen(port, () => console.log(`Server listening on my preferred port ${port}`));
+mongoose.connect('INSERT YOUR MONGODB KEY HERE')
+.then(()=> {
+    console.log('connected to mongoDB')
+
+    app.listen(port, () => console.log(`Server listening on my preferred port ${port}`));
+
+}).catch((error) => {
+    console.log(error)
+})
